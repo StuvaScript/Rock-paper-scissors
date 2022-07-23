7fb63fba79992be0c6e538459174c0de18e7594c
Stu <stumac1000@yahoo.com>
2022-07-22 17:04:39 -0700

    Fix loop issues I managed to fix the issue with the loop not cycling all 5 times and not resetting the computer's and player's choices.

--- javascript.js
+++ javascript.js
@@ -1,3 +1,4 @@
+
 // This is the computer's random choice generator.
 
 function getComputerChoice(min, max) {
@@ -13,8 +14,6 @@ function getComputerChoice(min, max) {
         return 'scissors';
     }
 }
-//console.log(getComputerChoice());
-
 
 
 // This is where the player chooses their weapon.
@@ -32,63 +31,101 @@ function getPlayerSelection() {
         wrongSelection();
     }
 }
-//console.log(getPlayerSelection());
-
-// Everything is good above this
 
 
+// This looping function encases the single round function and
+// plays multiple games.
 
-// This is the function to determine the round winner.
+function game() {
+    
+    let playerScore = 0;
+    let compScore = 0;
 
-const playerSelection = getPlayerSelection();
-const computerSelection = getComputerChoice();
+    for (let i = 0; i < 5; i++) {
+        console.log(i);
+            
+        // This is the function to determine the round winner.
 
-function playRound(playerSelection, computerSelection) {
-    if ((playerSelection == 'rock' && computerSelection == 'scissors')  || 
-       (playerSelection == 'paper' && computerSelection == 'rock') || 
-       (playerSelection == 'scissors' && computerSelection == 'paper')) {
-        return 'win';
-        //return win = alert(`You Win! ${playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1)} beats ${computerSelection.charAt(0).toUpperCase() + computerSelection.slice(1)}`);
-        } else if ((playerSelection == 'rock' && computerSelection == 'paper')  || 
-        (playerSelection == 'paper' && computerSelection == 'scissors') || 
-        (playerSelection == 'scissors' && computerSelection == 'rock')) {
-        return 'lose';
-            //return lose = alert(`You Lose! ${computerSelection.charAt(0).toUpperCase() + computerSelection.slice(1)} beats ${playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1)}`);
-       } else if (playerSelection === computerSelection) {
-        return 'draw';
-        //return alert('It\'s a draw!');
-       }
-}
+        function playRound(playerSelection, computerSelection) {
+            if ((playerSelection == 'rock' && computerSelection == 'scissors')  || 
+            (playerSelection == 'paper' && computerSelection == 'rock') || 
+            (playerSelection == 'scissors' && computerSelection == 'paper')) {
+                return 'win';
+                //return win = alert(`You Win! ${playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1)} beats ${computerSelection.charAt(0).toUpperCase() + computerSelection.slice(1)}`);
+                } else if ((playerSelection == 'rock' && computerSelection == 'paper')  || 
+                (playerSelection == 'paper' && computerSelection == 'scissors') || 
+                (playerSelection == 'scissors' && computerSelection == 'rock')) {
+                return 'lose';
+                    //return lose = alert(`You Lose! ${computerSelection.charAt(0).toUpperCase() + computerSelection.slice(1)} beats ${playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1)}`);
+            } else if (playerSelection === computerSelection) {
+                return 'draw';
+                //return alert('It\'s a draw!');
+            }
+        }
+
+        const playerSelection = getPlayerSelection();
+        const computerSelection = getComputerChoice();
+
+        console.log(playerSelection + ' player');
+        console.log(computerSelection + ' comp');
+        console.log(playRound(playerSelection, computerSelection) + ' round');
+        
+        function tally() {
+            if (playRound(playerSelection, computerSelection) == 'win') {
+                playerScore++;
+            } else if (playRound(playerSelection, computerSelection) == 'lose') {
+                compScore++;
+            }
+        }
+        tally();
+
+        console.log(playerScore + ' player score');
+        console.log(compScore + ' comp score');
+    }
+
+    if (playerScore > compScore) {
+        return alert('Winner!');
+    } else if (playerScore < compScore) {
+        return alert('Sorry you loser');
+    } else {
+        return alert('Draw!');
+    }
 
 
-//console.log(playerSelection + ' player');
-//console.log(computerSelection + ' comp');
-//console.log(playRound(playerSelection, computerSelection) + ' round');
+}   
+
 
 //const outcome = playRound(playerSelection, computerSelection);
 
 
-
-
-function game() {
+// function game() {
     
+//     let playerScore = 0;
+//     let compScore = 0;
 
-    for (let i = 0; i < 5; i++) {
-        console.log(i);
-        getComputerChoice();
-        console.log(computerSelection);
-        getPlayerSelection();
-        console.log(playerSelection);
-        playRound(playerSelection ,computerSelection);
-        console.log(playRound());
+//     for (let i = 0; i < 5; i++) {
+//         console.log(i);
+//         playRound();
+//         if (playRound() == 'win') {
+//             playerScore += 1;
+//         } else if (playRound() == 'lose') {
+//             compScore += 1;
+//         }
+//     getComputerChoice();
+//     getPlayerSelection();
         
-        
-    
-}
+//     }
 
-return playRound();
+//     if (playerScore > compScore) {
+//         return 'Winner!';
+//     } else if ( playerScore < compScore) {
+//         return 'You lose';
+//     } else {
+//         return 'Draw!';
+//     }
 
-}
+
+// }
 
 
 console.log(game());
