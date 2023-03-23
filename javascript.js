69de6fb70814918c89b94eaee781a9411a3215da
Stu <stumac1000@yahoo.com>
2023-03-22 22:32:43 -0700

    Clean up javascript file

--- javascript.js
+++ javascript.js
@@ -1,115 +1,25 @@
-// This is the click event and it wraps all functions
-
-const btns = document.querySelectorAll(".weapon");
-btns.forEach((weapon) =>
+const weapons = document.querySelectorAll(".weapon");
+weapons.forEach((weapon) =>
   weapon.addEventListener("click", (e) => {
-    if (
-      e.target.id != "rock" &&
-      e.target.id != "paper" &&
-      e.target.id != "scissors"
-    ) {
-      return;
-    }
-
     if (playerScore == 5 || compScore == 5) {
       return;
     }
-    // This is the computer's random choice generator
 
-    function getComputerChoice(min, max) {
-      min = 1;
-      max = 3;
-      let number = Math.floor(Math.random() * (max - min + 1)) + min;
-
-      if (number === 1) {
-        return "rock";
-      } else if (number === 2) {
-        return "paper";
-      } else {
-        return "scissors";
-      }
-    }
-
-    // This is where the player chooses their weapon based off the click event
-
-    function getPlayerSelection() {
-      let choice = e.target.id; // <= click event listener
-      if (choice == "rock" || choice == "paper" || choice == "scissors") {
-        return choice;
-      }
-    }
-
-    // This is the function to determine the round winner.
-
-    function playRound(playerSelection, computerSelection) {
-      if (
-        (playerSelection == "rock" && computerSelection == "scissors") ||
-        (playerSelection == "paper" && computerSelection == "rock") ||
-        (playerSelection == "scissors" && computerSelection == "paper")
-      ) {
-        return "win";
-      } else if (
-        (playerSelection == "rock" && computerSelection == "paper") ||
-        (playerSelection == "paper" && computerSelection == "scissors") ||
-        (playerSelection == "scissors" && computerSelection == "rock")
-      ) {
-        return "lose";
-      } else if (playerSelection === computerSelection) {
-        return "draw";
-      }
-    }
-
-    const playerSelection = getPlayerSelection();
+    const playerSelection = e.target.id;
     const computerSelection = getComputerChoice();
+    const round = playRound(playerSelection, computerSelection);
 
-    // This function keeps a running tally of each player's
-    // points.
-
-    function tally() {
-      if (playRound(playerSelection, computerSelection) == "win") {
-        playerScore++;
-        return (roundWin.innerText = `You Win! ${
-          playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1)
-        } beats ${
-          computerSelection.charAt(0).toUpperCase() + computerSelection.slice(1)
-        }`);
-      } else if (playRound(playerSelection, computerSelection) == "lose") {
-        compScore++;
-        return (roundWin.innerText = `You Lose! ${
-          computerSelection.charAt(0).toUpperCase() + computerSelection.slice(1)
-        } beats ${
-          playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1)
-        }`);
-      } else if (playRound(playerSelection, computerSelection) == "draw") {
-        return (roundWin.innerText = "It's a draw!");
-      }
-    }
-    tally();
-
-    // This part tells who the overall champion is.
-
-    function findWinner() {
-      if (playerScore == 5) {
-        champ = "Congrats! You are the ultimate winner!!!";
-        declareWinner();
-      } else if (compScore == 5) {
-        champ = "Sorry but you suck. Lolz";
-        declareWinner();
-      }
-    }
+    tally(round, playerSelection, computerSelection);
 
     findWinner();
 
-    const playerTotal = document.querySelector("#player");
     playerTotal.innerText = `${playerScore}`;
-
-    const compTotal = document.querySelector("#computer");
     compTotal.innerText = `${compScore}`;
   })
 );
 
-const roundWin = document.querySelector("#round-win");
-roundWin.innerText = "Choose your weapon!";
+const display = document.querySelector("#display");
+display.innerText = "Choose your weapon!";
 
 let champ = "";
 
@@ -118,10 +28,6 @@ const br = document.createElement("br");
 br.classList.add("newBreak");
 const div = document.createElement("div");
 div.classList.add("newDiv");
-
-function declareWinner() {
-  roundWin.innerText = `${champ}`;
-}
 
 let playerScore = 0;
 let compScore = 0;
@@ -140,10 +46,82 @@ function reset() {
   compScore = 0;
   compTotal.innerText = 0;
   playerTotal.innerText = 0;
-  roundWin.innerText = "Choose your weapon!";
-  if (!document.querySelector(".newBreak")) {
-    return;
+  display.innerText = "Choose your weapon!";
+}
+
+// This is the computer's random choice generator
+
+function getComputerChoice() {
+  min = 1;
+  max = 3;
+  let number = Math.floor(Math.random() * (max - min + 1)) + min;
+
+  switch (number) {
+    case 1:
+      return "rock";
+      break;
+    case 2:
+      return "paper";
+      break;
+    case 3:
+      return "scissors";
   }
-  container.removeChild(br);
-  container.removeChild(div);
+}
+
+// This is the function to determine the round winner.
+
+function playRound(playerSelection, computerSelection) {
+  if (
+    (playerSelection == "rock" && computerSelection == "scissors") ||
+    (playerSelection == "paper" && computerSelection == "rock") ||
+    (playerSelection == "scissors" && computerSelection == "paper")
+  ) {
+    return "win";
+  } else if (
+    (playerSelection == "rock" && computerSelection == "paper") ||
+    (playerSelection == "paper" && computerSelection == "scissors") ||
+    (playerSelection == "scissors" && computerSelection == "rock")
+  ) {
+    return "lose";
+  } else if (playerSelection === computerSelection) {
+    return "draw";
+  }
+}
+
+// This function keeps a running tally of each player's
+// points.
+
+function tally(round, playerSelection, computerSelection) {
+  switch (round) {
+    case "win":
+      playerScore++;
+      return (display.innerText = `You Win! ${
+        playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1)
+      } beats ${
+        computerSelection.charAt(0).toUpperCase() + computerSelection.slice(1)
+      }`);
+      break;
+    case "lose":
+      compScore++;
+      return (display.innerText = `You Lose! ${
+        computerSelection.charAt(0).toUpperCase() + computerSelection.slice(1)
+      } beats ${
+        playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1)
+      }`);
+      break;
+    case "draw":
+      return (display.innerText = "It's a draw!");
+  }
+}
+
+// This part tells who the overall champion is.  // This part tells who the overall champion is.
+
+function findWinner() {
+  if (playerScore == 5) {
+    champ = "Congrats! You rule!!!";
+    display.innerText = `${champ}`;
+  } else if (compScore == 5) {
+    champ = "Sorry but you suck. Lolz";
+    display.innerText = `${champ}`;
+  }
 }
